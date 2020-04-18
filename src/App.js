1a7dc2f0c01e444444c7d6e8910bd5b2e1aa222c
mdsaleem1804 <mdsaleem1804@gmail.com>
2020-04-18 19:37:26 +0530

    changes package.json

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-/*
+
 import FormPage from "./Components/FormRegister";
 import FormLogin from "./Components/FormLogin";
 
@@ -12,14 +12,14 @@ import FooterPage from "./Components/Common/FooterPage";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
 import "mdbreact/dist/css/mdb.css";
-<NavbarPage />
-        <PatientRegistration />
-        <FooterPage />
-*/
 function App() {
   return (
     <div className="App">
-      <h1>Saleem</h1>
+      <header>
+        <NavbarPage />
+        <PatientRegistration />
+        <FooterPage />
+      </header>
     </div>
   );
 }
