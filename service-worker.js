72721d8e36496356bc49a75ecb92ed2fb6d3dfc4
mdsaleem1804 <mdsaleem1804@gmail.com>
2020-04-18 18:58:52 +0530

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8d6a60c0c2f7ede1941a6e71146f30ae.js"
+  "/precache-manifest.cda5b0353c160c1452b214c170bc0ed7.js"
 );
 
 self.addEventListener('message', (event) => {
